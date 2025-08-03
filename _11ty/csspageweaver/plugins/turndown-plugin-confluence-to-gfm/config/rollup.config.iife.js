c0fd79dce93d393f43043f7d6f055da66f98e7b8
didierlechenne <didier@lechenne.fr>
2025-08-03 10:40:25 +0200

    clean

--- _11ty/csspageweaver/plugins/turndown-plugin-confluence-to-gfm/config/rollup.config.iife.js
+++
@@ -1,9 +0,0 @@
-import config from './rollup.config'
-
-export default config({
-  output: {
-    format: 'iife',
-    file: 'dist/turndown-plugin-confluence-to-gfm.js',
-    name: 'TurndownPluginConfluenceToGfmService'
-  }
-})
