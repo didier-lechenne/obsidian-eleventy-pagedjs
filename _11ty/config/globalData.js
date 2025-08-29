3c5ab700151528fc7f6452312f2fb3e809ccfb85
didierlechenne <didier@lechenne.fr>
2025-08-29 18:34:39 +0200

    up

--- _11ty/config/globalData.js
+++ _11ty/config/globalData.js
@@ -1,15 +1,17 @@
-eleventyConfig.addGlobalData("eleventyComputed", {
-  //  SINGLE PAGE
-  permalink: (data) => {
-    if (data.page.inputPath.endsWith("print.md")) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addGlobalData("eleventyComputed", {
+    //  SINGLE PAGE
+    permalink: (data) => {
+      if (data.page.inputPath.endsWith("print.md")) {
+        return data.permalink;
+      }
+      if (data.page.inputPath.endsWith("screen.md")) {
+        return data.permalink;
+      }
+      if (data.page.inputPath.endsWith(".md")) {
+        return false;
+      }
       return data.permalink;
-    }
-    if (data.page.inputPath.endsWith("screen.md")) {
-      return data.permalink;
-    }
-    if (data.page.inputPath.endsWith(".md")) {
-      return false;
-    }
-    return data.permalink;
-  },
-});
+    },
+  });
+};
