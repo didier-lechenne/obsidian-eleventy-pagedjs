b7f3db6535d548d22d739acab6c5018c4b477a5f
didierlechenne <didier@lechenne.fr>
2025-09-02 22:03:58 +0200

    up

--- _11ty/csspageweaver/plugins/editor/turndown-plugins/core-rules-plugin.js
+++ _11ty/csspageweaver/plugins/editor/turndown-plugins/core-rules-plugin.js
@@ -1,43 +1,40 @@
 export function coreRulesPlugin(turndownService) {
+  turndownService.keep(function (node) {
+    return node.nodeName === "DIV" && node.classList.contains("breakcolumn");
+  });
 
-turndownService.keep([
-  function (node) {
-    return node.nodeName === "SPAN" && node.style.getPropertyValue("--ls");
-  },
+  turndownService.keep([
+    function (node) {
+      return node.nodeName === "SPAN" && node.style.getPropertyValue("--ls");
+    },
 
+    "sup",
+    "sub",
+  ]);
 
-  "sup",
-  "sub",
-]);
+  // Sauvegarde la fonction escape originale
+  var originalEscape = turndownService.escape;
 
+  turndownService.escape = function (string) {
+    // Applique d'abord l'échappement standard
+    string = originalEscape.call(this, string);
 
+    // Puis ajoute vos échappements personnalisés
+    var customEscapes = [
+      [/\[\[/g, "\\[\\["],
+      [/\]\]/g, "\\]\\]"],
+    ];
 
+    return customEscapes.reduce(function (accumulator, escape) {
+      return accumulator.replace(escape[0], escape[1]);
+    }, string);
+  };
 
-// Sauvegarde la fonction escape originale
-var originalEscape = turndownService.escape;
-
-turndownService.escape = function (string) {
-  // Applique d'abord l'échappement standard
-  string = originalEscape.call(this, string);
-  
-  // Puis ajoute vos échappements personnalisés
-  var customEscapes = [
-    [/\[\[/g, "\\[\\["],
-    [/\]\]/g, "\\]\\]"],
-  ];
-
-  return customEscapes.reduce(function (accumulator, escape) {
-    return accumulator.replace(escape[0], escape[1]);
-  }, string);
-};
-
-
-// turndownService.addRule("blockquote", {
-//   filter: 'blockquote',
-//   replacement: function (content) {
-//     content = content.trim();
-//     return '\n\n> ' + content.replace(/\n/g, '\n> ') + '\n\n';
-//   }
-// });
-
+  // turndownService.addRule("blockquote", {
+  //   filter: 'blockquote',
+  //   replacement: function (content) {
+  //     content = content.trim();
+  //     return '\n\n> ' + content.replace(/\n/g, '\n> ') + '\n\n';
+  //   }
+  // });
 }
