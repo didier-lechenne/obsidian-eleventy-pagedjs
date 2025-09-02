3464e0e92c41a83266796ed44d6730bc620df492
didierlechenne <didier@lechenne.fr>
2025-09-02 21:30:45 +0200

    up

--- _11ty/csspageweaver/plugins/editor/turndown-plugins/break-plugin.js
+++ _11ty/csspageweaver/plugins/editor/turndown-plugins/break-plugin.js
@@ -8,24 +8,13 @@ export function breakPlugin(turndownService) {
     },
   });
 
-turndownService.addRule("breakcolumn", {
-  filter: function (node) {
-    return node.nodeName === "DIV" && node.classList.contains("breakcolumn");
-  },
-  replacement: function (content, node) {
-    // Clone le nœud pour éviter de modifier le DOM original
-    const cleanNode = node.cloneNode(false);
-
-    // Supprime tous les attributs `data-*`
-    Array.from(cleanNode.attributes).forEach(attr => {
-      if (attr.name.startsWith("data-")) {
-        cleanNode.removeAttribute(attr.name);
-      }
-    });
-
-    // Retourne la balise personnalisée
-    return "\n<breakcolumn>\n";
-  },
+ turndownService.addRule("breakcolumn", {
+    filter: function (node) {
+      return node.nodeName === "DIV" && node.classList.contains("breakcolumn");
+    }, 
+  replacement: function() {
+    return "<breakcolumn>";
+  }
 });
 
 
--- _11ty/csspageweaver/plugins/editor/turndown-plugins/index.js
+++ _11ty/csspageweaver/plugins/editor/turndown-plugins/index.js
@@ -5,6 +5,5 @@ export { footnotesPlugin } from './footnotes-plugin.js';
 export { spacesPlugin } from './spaces-plugin.js';
 export { annotationsPlugin } from "./annotations.js";
 export { _11tyPlugin } from "./_11ty.js";
-
 export { breakPlugin } from './break-plugin.js';
 export { coreRulesPlugin } from './core-rules-plugin.js';
