147af4b5fc4b2d31ca56b0eb625676b1eece39e2
didier <didier@lechenne.fr>
2025-09-06 12:37:35 +0200

    vault backup: 2025-09-06 12:37:35

--- .eleventy.js
+++ .eleventy.js
@@ -1,62 +1,65 @@
-const collectionsConfig = require("./_11ty/config/collections.js");
-const markdownPlugin = require("./_11ty/config/markdown.js");
-const imagePlugin = require("./_11ty/config/image.js");
-const filtersConfig = require("./_11ty/config/filters.js");
-const shortcodesConfig = require("./_11ty/config/shortcodes.js");
-const transformsConfig = require("./_11ty/config/transforms.js");
-const passthroughConfig = require("./_11ty/config/passthrough.js");
+
+// === CONFIGURATION DE BASE ===
 const yamlPlugin = require("./_11ty/config/yaml.js");
+const globalDataPlugin = require("./_11ty/config/globalData.js");
+const passthroughCopy = require("./_11ty/config/passthroughCopy.js");
+
+// === TRAITEMENT DU CONTENU ===
+const preprocessorConfig = require("./_11ty/config/preprocessor.js");
+const shortcodesConfig = require("./_11ty/config/mediaShortcodes.js");
+const markdownPlugin = require("./_11ty/config/markdown.js");
+
+// === COLLECTIONS ET FILTRES ===
+const collectionsConfig = require("./_11ty/config/collections.js");
+const filtersConfig = require("./_11ty/config/filters.js");
+
+// === POST-TRAITEMENT ===
+const transformsConfig = require("./_11ty/config/transforms.js");
+const afterBuild = require("./_11ty/config/afterBuild.js");
+
+// === CONFIGURATION SITE ===
 const config = require("./_11ty/config/siteData.js");
 
 module.exports = function (eleventyConfig) {
-  const env = process.env.ELEVENTY_ENV;
-
-
-  eleventyConfig.addPassthroughCopy({
-    [`${config.publicFolder}/images`]: "images",
-    [`_11ty/assets/themes/${config.theme}`]: "assets",
-    "_11ty/csspageweaver": "csspageweaver",
-  });
-
-  eleventyConfig.on("afterBuild", () => {
-    console.log("✅ Site screen généré avec succès !");
-  });
-
-  eleventyConfig.addGlobalData("eleventyComputed", {
-    permalink: (data) => {
-      
-      if (data.siteMode === "screen") {
-          return data.permalink;
-        }
-      
-      if (data.page.inputPath.endsWith(".md")) {
-        return false;
-      }
-      return data.permalink;
-    },
+  eleventyConfig.addTransform("fixImagePaths", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      // Transformer les chemins relatifs en chemins absolus
+      content = content.replace(/src="images\//g, 'src="./images/');
+      return content;
+    }
+    return content;
   });
 
   // === APPLIQUER LES CONFIGURATIONS ===
+  // 1. Configuration YAML 
+  // 2. Données globales
+  // 3. Copie des fichiers statiques
+  // 4. Préprocesseurs
+  // 5. Shortcodes (avant markdown)
+  // 6. Rendu Markdown
+  // 7. Collections
+  // 8. Filtres
+  // 9. Transformations
+  // 10. After Build 
 
   eleventyConfig.addPlugin(yamlPlugin);
-
-  if (env === "prod") {
-    console.log("✅ PROD !");
-    eleventyConfig.addPlugin(imagePlugin);
-  }
+  eleventyConfig.addPlugin(globalDataPlugin);
+  eleventyConfig.addPlugin(passthroughCopy);
+  preprocessorConfig(eleventyConfig);
+  shortcodesConfig(eleventyConfig);
   eleventyConfig.addPlugin(markdownPlugin);
   collectionsConfig(eleventyConfig);
   filtersConfig(eleventyConfig);
-  shortcodesConfig(eleventyConfig);
   transformsConfig(eleventyConfig);
-  passthroughConfig(eleventyConfig);
+  eleventyConfig.addPlugin(afterBuild);
 
   // === CONFIGURATION SERVEUR DE DEV ===
   eleventyConfig.setServerOptions({
     port: 3000,
+    watch: ["_11ty/**/*", config.publicFolder + "/**/*"],
     showAllHosts: true,
     domDiff: true,
-    ignored: ["node_modules/**", ".git/**", "**/.DS_Store"],
+    ignored: ["node_modules/**", ".git/**", "**/.DS_Store", "_site/**/*", "**/*.tmp"],
   });
 
   // === CONFIGURATION DES DOSSIERS ===
@@ -64,7 +67,7 @@ module.exports = function (eleventyConfig) {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
-    pathPrefix: "valentine",
+    pathPrefix: "",
     dir: {
       input: config.publicFolder,
       output: "_site/",
